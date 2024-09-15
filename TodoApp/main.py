e6a10a51aa1d0f2265bbf068d7b90d5b620ef8b9
maria-skygrid <maria.cosmen@skygrid.co.jp>
2024-09-16 08:46:05 +0900

    feature/update files

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,78 +1,10 @@
-from typing import Annotated
-from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends, HTTPException, Path
+from fastapi import FastAPI
 import models 
-from models import Todos
-from database import engine, SessionLocal
-from starlette import status
-from pydantic import BaseModel, Field
-from routers import auth
+from database import engine
+from api import api_router
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-app.include_router(auth.router)
-
-# only open the DB when we need to make changes to it and then close it. 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close
-
-# this db_dependency will be used in every endpoint
-db_dependency = Annotated[Session, Depends(get_db)]
-
-class TodoRequest(BaseModel):
-    title: str = Field(min_length=3)
-    description: str = Field(min_length=3, max_length=100)
-    priority: int = Field(gt=0, lt=6)
-    complete: bool    
-
-
-# INDEX
-@app.get('/', status_code=status.HTTP_200_OK)
-def index(db: db_dependency):
-    return db.query(Todos).all()
-
-# SHOW
-@app.get('/todo/{todo_id}', status_code=status.HTTP_200_OK)
-def show(db: db_dependency, todo_id: int=Path(gt=0)):
-    todo = db.get(Todos, todo_id)
-    if not todo:
-        raise HTTPException(status_code=404, detail='Element not')
-    return todo
-
-# POST
-@app.post('/todo/', status_code=status.HTTP_201_CREATED)
-def create(db: db_dependency, todo_request: TodoRequest):
-    todo = Todos(**todo_request.model_dump())
-    db.add(todo)
-    db.commit()
-
-# PUT
-@app.put('/todo/{todo_id}/edit', status_code=status.HTTP_204_NO_CONTENT)
-def update(
-    db: db_dependency, 
-    todo_request: TodoRequest,
-    todo_id: int = Path(gt=0),
-):
-    todo = todo_request.model_dump(exclude_unset=True)
-    updated_db = db.query(Todos).filter(Todos.id == todo_id).update(todo)
-    if not updated_db:
-        raise HTTPException(status_code=404, detail='Element not found')
-    db.commit()
-
-# DELETE
-@app.delete('/todo/{todo_id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete(
-    db: db_dependency, 
-    todo_id: int = Path(gt=0)
-):
-    todo = db.get(Todos, todo_id)
-    if not todo:
-        raise HTTPException(status_code=404, detail="Element not found")
-    db.delete(todo)
-    db.commit()
+app.include_router(api_router)
