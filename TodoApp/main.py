d34a89497eece7222d5bebb93d21c04c87bec70f
maria-skygrid <maria.cosmen@skygrid.co.jp>
2024-09-15 16:26:03 +0900

    feature/add show endpoint and validations

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,10 +1,10 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
 import models 
 from models import Todos
 from database import engine, SessionLocal
-
+from starlette import status
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
@@ -21,7 +21,17 @@ def get_db():
 # this db_dependency will be used in every endpoint
 db_dependency = Annotated[Session, Depends(get_db)]
 
-@app.get('/')
+# INDEX
+@app.get('/', status_code=status.HTTP_200_OK)
 def index(db: db_dependency):
     return db.query(Todos).all()
-    
+
+# SHOW
+@app.get('/todo/{todo_id}', status_code=status.HTTP_200_OK)
+def show(db: db_dependency, todo_id: int=Path(gt=0)):
+    todo = db.get(Todos, todo_id)
+    if not todo:
+        raise HTTPException(status_code=404, detail='Element not')
+    return todo
+
+
