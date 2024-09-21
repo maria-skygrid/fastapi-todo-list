a041c46d5fefdfb323166e488b576046b0f40ef6
maria-skygrid <maria.cosmen@skygrid.co.jp>
2024-09-21 10:43:57 +0900

    feature/add user index and create endpoints

--- TodoApp/auth/auth_router.py
+++ TodoApp/auth/auth_router.py
@@ -1,7 +1,46 @@
-from fastapi import APIRouter
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel, Field
+from database import SessionLocal
+from models import Users
+from pydantic import BaseModel, Field
 
 router = APIRouter()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+class CreateUserRequest(BaseModel):
+  email: str
+  username: str = Field(min_length=3, max_length=50)
+  first_name: str = Field(min_length=3)
+  last_name: str = Field(min_length=3)
+  password: str
+  role: str
+
+# INDEX
 @router.get('/auth/')
 def get_user():
   return { 'user': 'authenticated' }
+
+# CREATE
+@router.post('/auth')
+def create(create_user_request: CreateUserRequest):
+  user = Users(
+    email=create_user_request.email,
+    username=create_user_request.username, 
+    first_name=create_user_request.first_name,
+    last_name=create_user_request.last_name,
+    role=create_user_request.role, 
+    hashed_password=create_user_request.password,
+    is_active=True
+  )
+
+  return user
