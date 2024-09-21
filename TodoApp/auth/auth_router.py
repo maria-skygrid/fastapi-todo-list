0549bc40a47a823e0a234f9d9cc026aaed779a89
maria-skygrid <maria.cosmen@skygrid.co.jp>
2024-09-21 19:07:22 +0900

    feature/add user authentication on login

--- TodoApp/auth/auth_router.py
+++ TodoApp/auth/auth_router.py
@@ -7,10 +7,23 @@ from starlette import status
 from models import Users
 from pydantic import BaseModel, Field
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
-bcript_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+def authenticate_user(username: str, password: str, db):
+   user = db.query(Users).filter(Users.username == username).first()
+   
+   if not user:
+      return False
+   
+   if not bcrypt_context.verify(password, user.hashed_password):
+      return False
+   
+   return True
+   
 
 def get_db():
     db = SessionLocal()
@@ -43,8 +56,16 @@ def create(db: db_dependency, create_user_request: CreateUserRequest):
     first_name=create_user_request.first_name,
     last_name=create_user_request.last_name,
     role=create_user_request.role, 
-    hashed_password=bcript_context.hash(create_user_request.password),
+    hashed_password=bcrypt_context.hash(create_user_request.password),
     is_active=True
   )
   db.add(user)
   db.commit()
+
+# LOGIN
+@router.post('/token')
+def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+       return 'Authentication failed'
+    return 'Succesful Authentication'
