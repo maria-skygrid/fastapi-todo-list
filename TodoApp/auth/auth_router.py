b5aebca7e24b4652ef4cde1f709e0508131b1caa
maria-skygrid <maria.cosmen@skygrid.co.jp>
2024-09-22 22:16:08 +0900

    fix/ fix algorithm typo

--- TodoApp/auth/auth_router.py
+++ TodoApp/auth/auth_router.py
@@ -47,7 +47,7 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
 # we call this function to all API endpoints that need authorization. 
 def get_current_user(token: Annotated[str, Depends(oauth_bearer)]):
    try: 
-      payload = jwt.decode(token, SECRET_KEY, algorithm=[ALGORITHM])
+      payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
       username: str = payload.get('sub') #sub is our username
       user_id: int = payload.get('id')
       if username is None or user_id is None:
